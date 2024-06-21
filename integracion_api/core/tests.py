cb3f9f6ff83d879062c5f196994538e438176a49
Nicolás Muñoz <n.munoz7@duocuc.cl>
2024-06-21 17:47:58 -0400

    chore: Agregar pruebas de integración para la vista de pedidos

--- integracion_api/core/tests.py
+++ integracion_api/core/tests.py
@@ -1,4 +1,3 @@
-import requests
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
@@ -126,5 +125,55 @@ class UserViewTest(TestCase):
         response = self.client.get(self.url + '1/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     
+class PedidoViewTest(TestCase):
+    def setUp(self):
+        tipo_usuario, created = TipoUsuario.objects.get_or_create(id_tipo_usuario=1) 
+        self.client = APIClient()
+        self.user = UsuarioPersonalizado.objects.create_user(username='testuser', password='testpassword',id_tipo_usuario=tipo_usuario)
+        self.user_id = self.user.id
+        self.estado = Estado.objects.create(nombre='Estado de prueba')
+        self.estado_id = self.estado.id_estado
+        self.token = Token.objects.create(user=self.user)
+        self.url = reverse('pedido-list')  
     
+    def test_missing_authorization_header(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
     
+    def test_create_pedido(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        data = {
+            'usuario': self.user_id,
+            'fecha': '2022-01-01',
+            'estado': self.estado_id
+        }
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_get_pedidos(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_update_pedido(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        data = {
+            'usuario': self.user_id,
+            'fecha': '2022-01-02',
+            'estado': self.estado_id
+        }
+        response = self.client.put(self.url, data)
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    def test_get_pedido(self):
+        self.test_create_pedido()
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        url = f'{self.url}1/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_delete_pedido(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
